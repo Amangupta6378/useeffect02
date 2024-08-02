2e886a78501e2fd98c3a58266722540af3440525
vikas <shahv7411@gmail.com>
2024-08-02 15:34:05 +0530

    useEffect2

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,26 +1,27 @@
-import React from 'react'
+import React from "react";
 
- const Filter = ({FilterData, category, setCategory}) => {
-
-  function filterHandler(title){
-    setCategory(title)  
+const Filter = ({ FilterData, category, setCategory }) => {
+  function filterHandler(title) {
+    setCategory(title);
   }
-  
+
   return (
-    <div className='w-11/12 flex flex-wrap max-w-max space-x-4 gap-y-4 mx-auto py-4 justify-center'>
-   { 
-    FilterData.map((data)=>{
+    <div className="w-11/12 flex flex-wrap max-w-max space-x-4 gap-y-4 mx-auto py-4 justify-center">
+      {FilterData.map((data) => {
         return (
-            <button 
-            className={'text-lg px-2 py-1 rounded font-medium text-white bg-black hover:bg-opacity-50 border-2 transition-all duration-300 '} 
-            key={data.id} onClick={()=>filterHandler(data.title)}>
-            
-            {data.title}</button>
-        )
-    }) 
-   }
+          <button
+            className={
+              "text-lg px-2 py-1 rounded font-medium text-white bg-black hover:bg-opacity-50 border-2 transition-all duration-300 "
+            }
+            key={data.id}
+            onClick={() => filterHandler(data.title)}
+          >
+            {data.title}
+          </button>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
 export default Filter;
